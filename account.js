c1f48e310c9bc7fb9c98f6f6926f8242930318e8
Dallas Sosebee <dallas Sosebee>
2021-10-18 10:55:57 -0500

    Add requiremtnts to import lower files into the higher files.

---
+++ account.js
@@ -0,0 +1,6 @@
+"use strict"
+
+
+
+module.exports.pin = userPin;
+module.exports.uBal = balance;
---
+++ atm.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const account = require("./account");
+
+
+
+
+
+
+
+
+
+
+
+module.exports.pinChecker = correctPin;
+module.exports.newBal = getBal
+module.exports.atmwithdraw = getWithdrawal;
+module.exports.atmdeposit = letDeposit;
